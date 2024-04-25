b5c3df23cac246de06970c9b2146caea2a1fc64d
Matheus Amon <amonmatheus757@gmail.com>
2024-04-25 11:44:10 -0300

    feat(database): prepara o ambiente para producao

--- src/database/index.js
+++ src/database/index.js
@@ -1,11 +1,9 @@
 //a biblioteca pg-promise retorna uma função, por isso já importamos e executamos simultaneamente
 const pgp = require('pg-promise')()
 const { join } = require('node:path')
+require("dotenv").config()
 
-const USER = process.env.POSTGRESQL_USER
-const PASSWORD = process.env.DB_PASSWORD
-const DATABASE = process.env.DATABASE
-const db = pgp(`postgres://${USER}:${PASSWORD}@localhost:5432/${DATABASE}`)
+const db = pgp(process.env.DATABASE_URL)
 
 //db.query("SELECT 1 + 1 AS result").then(result => console.log(result))
 
