51633906d917af60c0eb2e327f8754e140d228b1
Matheus Amon <amonmatheus757@gmail.com>
2024-05-20 08:50:40 -0300

    chore: update project dependencies

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const app = require("./app")
+
+const PORT = process.env.PORT || 10001
+
+const start = async () => {
+    try{
+        app.listen({ port: PORT })
+        app.log.info(`Server is running on https://localhost:${PORT}`)
+    } catch(err){
+        app.log.error(err)
+        process.exit(1)
+    }
+}
+
+start()
